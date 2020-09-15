739d76476b51cde0d268e7b7e3c33cb0ade9f09f
Rohan Naroth <rohan.naroth@gmail.com>
2020-09-15 23:15:13 +0000

    added html

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-console.log("hi")
+var num1;
+var num2;
 
 function add(n1, n2) {
-  var sum = n1 + n2
-  console.log(sum);
+    var sum = n1 + n2
+    console.log(sum);
 
-  document.getElementById("sum").innerHTML = sum;
-
+    document.getElementById("sum").innerHTML = sum;
 }
 
-add(2,2)
+add(2,4)
+
+function clicked(num) {
+  
+
+    if (num1 == null) {
+        num1 = num;
+        console.log(num1);
+    }
+
+    else if (num1 !== null) {
+        num2 = num;
+    }
+    console.log(num2)
+
+    // document.getElementsByClassName("num").innerHTML = "You clicked me!";
+
+    // num1 = 1;
+    // console.log(num1)
+
+    // console.log(document.getElementsByClassName("num")[2].innerHTML)
+    
+}
+
+
